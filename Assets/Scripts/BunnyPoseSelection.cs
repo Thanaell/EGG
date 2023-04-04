84cb540a5e32882e8147cc8554024154245945ca
Thanaell <gaelle_clavelin@orange.com>
2023-04-04 23:24:26 +0200

    static gesture recognition (with SDK or custom)

---
+++ Assets/Scripts/BunnyPoseSelection.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyPoseSelection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void bunnyRecognized(){
+        Debug.Log("bunny pose detected");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
